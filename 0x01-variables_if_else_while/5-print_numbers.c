097de1ff0c23d5072c3da156ff7735c3da327a73
iel-wafi <ielwafi281@gmail.com>
2023-03-17 16:17:21 -0700

    Print alp/num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+int             main(void)
+{
+        write(1, "0123456789\n", 11);
+        return (0);
+}                              
