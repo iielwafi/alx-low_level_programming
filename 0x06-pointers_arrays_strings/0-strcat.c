c2e65de27528cd5e444dda467cab313930c540a6
iel-wafi <ielwafi281@gmail.com>
2023-03-29 17:18:12 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to resulting string dest
+ */
 char	*_strcat(char *dest, char *src)
 {
 	int	i;
