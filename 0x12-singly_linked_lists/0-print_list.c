6a45b0f4db86d7778e50190f94da7a6b87ad2c88
iel-wafi <ielwafi281@gmail.com>
2023-04-28 05:17:53 -0700

    fix bug

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t x = 0;
+	size_t number = 0;
 
 	while (h)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		x++;
+		number++;
 	}
 
-	return (x);
+	return (number);
 }
