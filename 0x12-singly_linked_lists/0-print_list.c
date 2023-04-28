119158f3ad423696ec5538a4d6a182accd858a56
iel-wafi <ielwafi281@gmail.com>
2023-04-27 20:17:45 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,18 +7,22 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t x = 0;
 
 	while (h)
 	{
-		if (h->str)
-			printf("[%u] %s\n", h->len, h->str);
-		else
+		if (h->str != NULL)
 			printf("[0] (nil)\n");
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
 		h = h->next;
 		x++;
 	}
+
 	return (x);
 }
