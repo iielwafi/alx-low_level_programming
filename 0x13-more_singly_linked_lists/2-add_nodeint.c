79a489fe585acf8b3ffaa71aba89a1eb11a2581c
iel-wafi <ielwafi281@gmail.com>
2023-05-02 14:09:51 -0700

    task02

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that get a new node at the fist list
+ *
+ * @head: ptr to fist node
+ * @n: int n to add a new node
+ *
+ * Return: address of new digit or NULL if get fail
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *x;
+
+	x = malloc(sizeof(listint_t));
+	if (x == NULL)
+		return (NULL);
+	else
+		x->next = *head;
+
+	x->n = n;
+	*head = x;
+	return (x);
+}
