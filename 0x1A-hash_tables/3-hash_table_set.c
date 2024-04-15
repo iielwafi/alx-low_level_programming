ce8ee9a5b36b9c4e7662f924d8fdd5962f8b4f8c
iielwafi <ismail.elwafi1234@gmail.com>
2024-04-15 06:50:33 -0700

    fix betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL, *tmp = NULL;
 	unsigned long int index = 0;
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 || key == NULL || strlen(key) == 0)
+	if (ht == NULL || ht->array == NULL || ht->size == 0
+	|| key == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
